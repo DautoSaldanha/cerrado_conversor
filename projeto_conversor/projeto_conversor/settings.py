9843af0a1b14cd3d89147118d1c5faea202087f9
DautoSaldanha <dautosaldanhaalves28@gmail.com>
2026-04-20 21:55:40 -0300

    a

--- projeto_conversor/projeto_conversor/settings.py
+++ projeto_conversor/projeto_conversor/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y-_#k-!7$u8emuk+r_y9s5*k-ydr@@&j8*be29t6c5_v@(f36*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
