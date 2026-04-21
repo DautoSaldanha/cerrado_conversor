65db630430a90a6accf0d55520fab06e284bb4ab
DautoSaldanha <dautosaldanhaalves28@gmail.com>
2026-04-20 22:32:17 -0300

    b

--- projeto_conversor/projeto_conversor/asgi.py
+++ projeto_conversor/projeto_conversor/asgi.py
@@ -11,6 +11,5 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_conversor.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_conversor.projeto_conversor.settings')
 application = get_asgi_application()
